19ad2d3368a5aaef36aaa58eb31c3475b38924bf
Snuffy2 <Snuffy2@users.noreply.github.com>
2023-07-30 00:03:57 -0400

    Update openvpn_otp_auth.py

--- openvpn_otp_auth.py
+++ openvpn_otp_auth.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python3
 """OpenVPN OTP auth script.
-Version: 1.1
+Version: v1.1
 
 Author: @Snuffy2
 Initial Author: @roman-vynar
 Expanded from: https://github.com/roman-vynar/random-scripts
 
-Validates OpenVPN username/password/TOTP from file passed as the first arg when called using auth-user-pass-verify.
-TOTP/2FA/MFA uses Google Authenticator (or Authenticator-supporting third-party applications)
-User management is done from the CLI and stores users credentials and sessions in SQLite DBs.
-
-Use cases:
-1. Initial connect or after manual disconnect - a new OTP session record is created.
-2. If using auth-gen-token with external-auth - a user session is validated.
-
-Changelog:
-1.0: Initial @Snuffy2 release, expanded from @roman-vynar
-1.1: Support external-auth
+https://github.com/Snuffy2/openvpn_otp_auth
 """
 import argparse
 import base64
